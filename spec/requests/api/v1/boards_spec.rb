ff19f2d42562a70282c3d6babd0aede82ccf8e00
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-03 20:44:13 +0200

    Add test for boards request

---
+++ spec/requests/api/v1/boards_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Boards API' do
+  it 'returns a list of boards' do
+    create_list(:board, 10)
+    
+    get '/api/v1/boards'
+    json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json.length).to eq(10)
+  end
+end
