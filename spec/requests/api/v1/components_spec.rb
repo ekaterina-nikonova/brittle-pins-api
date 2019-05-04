82c43d25b47172c1a04cd2a28c68471ff17987ed
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-04 12:37:04 +0200

    Write a test for components show action

--- spec/requests/api/v1/components_spec.rb
+++ spec/requests/api/v1/components_spec.rb
@@ -2,15 +2,30 @@
 
 require 'rails_helper'
 
-describe 'Components API' do
-  it 'returns a list of components' do
-    board = build(:board)
-    create_list(:component, 20, boards: [board])
-    
-    get '/api/v1/components'
-    json = JSON.parse(response.body)
+RSpec.describe Api::V1::ComponentsController, type: :controller do
+  describe 'GET index' do
+    it 'returns a list of components' do
+      board = build(:board)
+      create_list(:component, 20, boards: [board])
+      
+      get :index
+      json = JSON.parse(response.body)
+  
+      expect(response).to be_successful
+      expect(json.length).to eq(20)
+    end
+  end
 
-    expect(response).to be_successful
-    expect(json.length).to eq(20)
+  describe 'GET show' do
+    it 'retrieves a component' do
+      component = build(:component)
+      component.save
+
+      get :show, params: { id: component.id }
+      json = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(json['name']).to eq('test component name')
+    end
   end
 end
