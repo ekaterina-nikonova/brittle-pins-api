72a872ff18f1eebe85c28b2d2207a1847303455a
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-04 11:25:26 +0200

    Write tests for board create and update actions

--- spec/requests/api/v1/boards_spec.rb
+++ spec/requests/api/v1/boards_spec.rb
@@ -29,4 +29,30 @@ RSpec.describe Api::V1::BoardsController, type: :controller do
       expect(json["image"]).not_to be_nil
     end
   end
+
+  describe 'POST create' do
+    it 'creates a board' do
+      name = ('a'..'z').to_a.shuffle[0, 10].join
+      description = ('a'..'z').to_a.shuffle[0, 20].join
+
+      post :create, params: { board: { name: name,
+                                       description: description } }
+      expect(Board.last.name).to eq(name)
+      expect(Board.last.description).to eq(description)
+    end
+  end
+
+  describe 'PATCH update' do
+    it 'updates a board' do
+      post :create, params: { board: { name: 'name 1',
+                                       description: 'description 1'} }
+      id = Board.last.id
+
+      patch :update, params: {id: id, board: { 
+                                        name: 'name 2',
+                                        description: 'description 2'} }
+      expect(Board.last.name).to eq('name 2')
+      expect(Board.last.description).to eq('description 2')
+    end
+  end
 end
