c4b4f1a9444c59daa74b53246d4b8f58b34ebd2d
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-04 11:01:10 +0200

    Add test for board show action

--- spec/requests/api/v1/boards_spec.rb
+++ spec/requests/api/v1/boards_spec.rb
@@ -2,14 +2,31 @@
 
 require 'rails_helper'
 
-describe 'Boards API' do
-  it 'returns a list of boards' do
-    create_list(:board, 10)
+RSpec.describe Api::V1::BoardsController, type: :controller do
+  let(:board_with_image) { build :board, :with_image }
 
-    get '/api/v1/boards'
-    json = JSON.parse(response.body)
+  describe 'GET index' do
+    it 'returns a list of boards' do
+      create_list(:board, 10)
+  
+      get :index
+      json = JSON.parse(response.body)
+  
+      expect(response).to be_successful
+      expect(json.length).to eq(10)
+    end
+  end
 
-    expect(response).to be_successful
-    expect(json.length).to eq(10)
+  describe 'GET show' do
+    it 'retrieves a specific board' do
+      board_with_image.save
+      id = Board.last.id
+      
+      get :show, params: { id: id }
+      json = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(json["image"]).not_to be_nil
+    end
   end
 end
