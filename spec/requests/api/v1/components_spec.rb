dfce6f0c646a27a92ed487a98a193383065b3d46
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-04 13:40:25 +0200

    Write a test for controller update action with an error

--- spec/requests/api/v1/components_spec.rb
+++ spec/requests/api/v1/components_spec.rb
@@ -48,4 +48,28 @@ RSpec.describe Api::V1::ComponentsController, type: :controller do
       expect(response).not_to be_successful
     end
   end
+
+  describe 'PATCH update' do
+    it 'updates a component' do
+      component = build(:component)
+      component.save
+  
+      patch :update, params: {
+        id: component.id,
+        component: { name: 'new name', description: 'new description' }
+      }
+      expect(Component.last.name).to eq('new name')
+      expect(Component.last.description).to eq('new description')
+    end
+
+    it 'can return an error' do
+      component = build(:component)
+      component.save
+
+      allow_any_instance_of(Component).to receive(:update).and_return(false)
+      patch :update, params: { id: component.id,
+                               component: { name: 'another name' } }
+      expect(response).not_to be_successful
+    end
+  end
 end
