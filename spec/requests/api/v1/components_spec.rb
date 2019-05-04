318e217daf29374de806783e02d3cc8c8f93f43c
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-04 12:44:38 +0200

    Write a test for controllers create action with an error

--- spec/requests/api/v1/components_spec.rb
+++ spec/requests/api/v1/components_spec.rb
@@ -28,4 +28,24 @@ RSpec.describe Api::V1::ComponentsController, type: :controller do
       expect(json['name']).to eq('test component name')
     end
   end
+
+  describe 'POST create' do
+    it 'creates a component' do
+      name = ('a'..'z').to_a.shuffle[0, 10].join
+      description = ('a'..'z').to_a.shuffle[0, 20].join
+
+      post :create, params: { component: { name: name,
+                                           description: description } }
+
+      expect(response).to be_successful
+      expect(Component.last.name).to eq(name)
+      expect(Component.last.description).to eq(description)
+    end
+
+    it 'can return an error' do
+      allow_any_instance_of(Component).to receive(:save).and_return(false)
+      post :create, params: { component: { name: 'n', description: 'd' } }
+      expect(response).not_to be_successful
+    end
+  end
 end
