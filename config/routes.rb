e8fc36ec61c0f91d059b1b96ad1813c2f65cd90d
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-10 20:06:26 +0200

    Add Signin controller; build create action, write tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'signup', to: 'signup#create'
+      post 'signin', to: 'signin#create'
 
       resources :boards do
         get 'components' => :components
