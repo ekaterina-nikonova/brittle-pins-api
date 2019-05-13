6db321cf4bc6e14bf94cc748dd33bf4a32d73006
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-13 13:45:31 +0200

    Add keys for JWTSessions

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -3,3 +3,7 @@
 key = Rails.env.test? ? SecureRandom.hex(24) : Rails.application.secrets.secret_key_base
 
 JWTSessions.encryption_key = key
+
+JWTSessions.algorithm = 'RS256'
+JWTSessions.private_key = OpenSSL::PKey::RSA.generate(2048)
+JWTSessions.public_key = JWTSessions.private_key.public_key
