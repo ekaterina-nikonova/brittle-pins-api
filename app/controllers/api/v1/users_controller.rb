740914f7327c33d75af8dd8159919e4058e4e188
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-07-15 13:12:40 +0200

    Create me endpoint for current user's id, name, email, and role

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Generated for creating 'me' endpoint
+# (see https://medium.com/@yuliaoletskaya/rails-api-jwt-auth-vuejs-spa-part-2-roles-601e4372a7e7)
+
+class Api::V1::UsersController < ApplicationController
+  def me
+    render json: current_user.as_json(only: [:id, :email, :username, :role])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'users/me'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       post 'signup', to: 'signup#create'
       post 'signin', to: 'signin#create'
       post 'refresh', to: 'refresh#create'
       delete 'signin', to: 'signin#destroy'
+      
+      get 'me', controller: :users, action: :me
 
       resources :boards do
         get 'components' => :components
