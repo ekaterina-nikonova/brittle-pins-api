d4c4767a0055351b223a0be88160885e49e0b44a
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-15 00:57:37 +0200

    Add email verification for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { email.downcase! }
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+
+  validates :email,
+            presence: true,
+            format: { with: EMAIL_REGEX, on: :create },
+            length: { maximum: 255 },
+            uniqueness: { case_sensitive: false }
+
   has_many :boards
   has_many :components
 
