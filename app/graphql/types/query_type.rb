df51b2ad5d4e2d26a26e1b38d23929c5ebcda5fd
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-18 00:12:22 +0100

    Add Board type, query for getting boards

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,11 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :boards, [BoardType], null: true
+    def boards
+      context[:current_user].boards
+    end
+
     field :projects, [ProjectType], null: true
     def projects
       context[:current_user].projects
