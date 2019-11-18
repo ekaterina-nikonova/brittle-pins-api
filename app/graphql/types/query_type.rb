34ba7f45578b3f4e0de96138137bd0ffc8290241
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-18 01:36:58 +0100

    Create Component type; add query for getting components for a board

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,6 +8,13 @@ module Types
       context[:current_user].boards
     end
 
+    field :componentsForBoard, [ComponentType], null: true do
+      argument :boardId, ID, required: true
+    end
+    def components_for_board(board_id:)
+      context[:current_user].boards.find(board_id).components
+    end
+
     field :projects, [ProjectType], null: true
     def projects
       context[:current_user].projects
