11488b2f83750e7974fd090036c9a7b8225b0319
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-18 02:57:36 +0100

    Fix Create Project mutation (get rid of Relay)

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 module Mutations
-  class BaseMutation < GraphQL::Schema::RelayClassicMutation
+  class BaseMutation < GraphQL::Schema::Mutation
     object_class Types::BaseObject
     field_class Types::BaseField
-    input_object_class Types::BaseInputObject
   end
 end
--- app/graphql/mutations/create_project.rb
+++ app/graphql/mutations/create_project.rb
@@ -4,28 +4,33 @@ module Mutations
   class CreateProject < BaseMutation
     null true
 
-    argument :board_id, ID, required: true
+    argument :boardId, ID, required: true
     argument :name, String, required: true
+    argument :description, String, required: false
+    argument :components, [String], required: false
 
     field :project, Types::ProjectType, null: true
     field :errors, [String], null: false
 
-    def resolve(name: '', board_id:)
-      pp '---user---'
-      pp context[:current_user]
-
+    def resolve(name: '', board_id:, description:, components:)
       user = context[:current_user]
-
-      # return { project: nil, errors: ['Not authorized'] } unless user
+      return { project: nil, errors: ['Not authorized'] } unless user
 
       project = user.projects.build(name: name,
-                                    board: user.boards.find(board_id))
-
+                                    board: user.boards.find(board_id),
+                                    description: description,
+                                    components: find_for(user, components))
       if project.save
         { project: project, errors: [] }
       else
         { project: nil, errors: project.errors.full_messages }
       end
+    end
+
+    private
+
+    def find_for(user, components)
+      components.map { |id| user.components.find(id) }
     end
   end
 end
