35b9cef507ce35b5b6e7a3473adb0ee332848fa1
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-21 14:01:28 +0100

    Create Delete Project mutation with a subscription

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
+    field :delete_project, mutation: Mutations::DeleteProject
   end
 end
--- app/graphql/types/subscription_type.rb
+++ app/graphql/types/subscription_type.rb
@@ -2,5 +2,8 @@ module Types
   class SubscriptionType < BaseObject
     field :projectAdded, Types::ProjectType, null: false
     def project_added; end
+
+    field :projectDeleted, Types::ProjectType, null: true
+    def project_deleted; end
   end
 end
