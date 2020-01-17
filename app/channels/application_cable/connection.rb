b1e41156de570342b30be19a7b36588ceefa72d4
ekaterina <ekaterina.v.nikonova@gmail.com>
2020-01-17 12:32:08 +0100

    Set channel user to nil if no user ID in cookies

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,10 @@ module ApplicationCable
     identified_by :channel_user
 
     def connect
-      @channel_user = User.find(cookies[:user_id])
+      @channel_user = nil
+      return unless cookies[:user_id]
+
+      @channel_user = User.find(cookies[:user_id]) if cookies[:user_id]
     end
   end
 end
