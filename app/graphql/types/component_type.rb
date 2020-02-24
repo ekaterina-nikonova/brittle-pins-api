911ee469a845ad74d0441c05a1b3424be2817e6e
EN <ekaterina.v.nikonova@gmail.com>
2020-02-24 15:24:55 +0100

    Add image url to Component type

--- app/graphql/types/component_type.rb
+++ app/graphql/types/component_type.rb
@@ -3,5 +3,12 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :description, String, null: true
+    field :image_url, String, null: true
+
+    def image_url
+      return '' unless object.image.attached?
+
+      Rails.application.routes.url_helpers.rails_blob_url(object.image)
+    end
   end
 end
--- app/models/component.rb
+++ app/models/component.rb
@@ -6,6 +6,7 @@ class Component < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :boards, uniq: true
   has_and_belongs_to_many :projects, uniq: true
+  has_one_attached :image
 
   private
 
