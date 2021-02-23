4c6669401ba62534acfb1ce81d964df24d5d9d88
EN <ekaterina.v.nikonova@gmail.com>
2021-02-23 16:32:45 +0100

    Accept "public" argument on project create

--- app/graphql/mutations/create_project.rb
+++ app/graphql/mutations/create_project.rb
@@ -4,21 +4,23 @@ module Mutations
   class CreateProject < BaseMutation
     null true
 
-    argument :boardId, ID, required: true
     argument :name, String, required: true
     argument :description, String, required: false
+    argument :public, Boolean, required: false
+    argument :boardId, ID, required: true
     argument :components, [String], required: false
 
     field :project, Types::ProjectType, null: true
     field :errors, [String], null: false
 
-    def resolve(name: '', board_id: '', description: '', components: [])
+    def resolve(name: '', public: false, board_id: '', description: '', components: [])
       user = context[:current_user]
       return { project: nil, errors: ['Not authorized'] } unless user
 
       project = user.projects.build(name: name,
-                                    board: user.boards.find(board_id),
                                     description: description,
+                                    public: public,
+                                    board: user.boards.find(board_id),
                                     components: find_for(user, components))
       if project.save
         { project: project, errors: [] }
